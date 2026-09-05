86eb97f1ffebf5dbe80b0573c0f5e97db263253d
Dipankar Jana <dipankarj571@gmail.com>
2026-09-05 21:41:03 +0530

    vite configuration

--- community-service/vite.config.js
+++ community-service/vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Community_Service_Application/',
+  base: '/Community_Service_Application',
 })
